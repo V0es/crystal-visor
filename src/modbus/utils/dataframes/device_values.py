8f95571614212c885d5567e5b21ff86a0fb0019e
v0es <markkikov@gmail.com>
2024-11-11 12:02:51 +0300

    slight fixes

--- src/modbus/utils/dataframes/device_values.py
+++ src/modbus/utils/dataframes/device_values.py
@@ -8,6 +8,6 @@ from ..enums import OperatingMode
 @dataclass
 class DeviceValues:
     current_operating_mode: int = OperatingMode.STOP
-    current_program: TemperatureProgram = field(default_factory= lambda: TemperatureProgram())
+    current_program: TemperatureProgram = field(default_factory=lambda: TemperatureProgram())
     current_temperature: int = 0
     current_point_position: int = 0
---
+++ test/mock/__init__.py
@@ -0,0 +1,3 @@
+from .image_analysis_mock import ImageAnalysisMock
+from .camera_mock import CameraMock
+from .trm_mock import TrmMock
