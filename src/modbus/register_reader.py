ecd633dfdfa167067fb9c1b0a1375311073558a4
v0es <markkikov@gmail.com>
2024-11-14 23:48:38 +0300

    moved to register and register_map dataclasses

--- src/modbus/register_reader.py
+++ src/modbus/register_reader.py
@@ -6,6 +6,7 @@ from PyQt6.QtCore import QThread, pyqtSignal, pyqtSlot
 from pymodbus.client import ModbusSerialClient
 
 from src.modbus.exceptions import ReadRegistersError
+from src.modbus.register_map import RegisterMap, Register
 from src.modbus.utils.dataframes import DeviceValues, TemperatureProgram
 
 logger = logging.getLogger(__name__)
@@ -17,30 +18,39 @@ class RegisterReaderThread(QThread):
     modbus_connection_lost = pyqtSignal()
     read_registers_error = pyqtSignal()
 
-    def __init__(self, modbus_client: ModbusSerialClient, slave_id: int):
+    def __init__(self,
+                 registers: RegisterMap,
+                 modbus_client: ModbusSerialClient,
+                 slave_id: int,
+                 polling_rate_msec: int = 1000):
+
         super().__init__()
-        self._is_running = False
-        self.modbus = modbus_client
-        self.slave_id = slave_id
+        self.is_running = False
         self._current_values_buffer = DeviceValues()
 
+        self.registers = registers
+        self.modbus = modbus_client
+        self.slave_id = slave_id
+        self.polling_rate_sec = polling_rate_msec / 1000
+
     def run(self):
-        self._is_running = True
-        while self._is_running:
+        self.is_running = True
+        while self.is_running:
+            time.sleep(self.polling_rate_sec)
 
             logger.info('getting device values')
 
             current_operating_mode = self.get_current_operation_mode()
             print(f'operation mode: address 17 = {current_operating_mode}')
-            time.sleep(0.01)
+            time.sleep(0.2)
 
             current_temperature = self.get_current_temperature()
             print(f'current_temperature: address 2 = {current_temperature}')
-            time.sleep(0.01)
+            time.sleep(0.2)
 
             current_program = self.get_current_temperature_program()
             print(f'current program: address 256 = {current_program}')
-            time.sleep(0.01)
+            time.sleep(0.2)
 
             current_point_position = self.get_current_point_position()
             print(f'current point position: address 0 = {current_point_position}\n')
@@ -53,11 +63,10 @@ class RegisterReaderThread(QThread):
             )
 
             self.result.emit(device_values)
-            time.sleep(1)
 
     @pyqtSlot()
     def stop(self):
-        self._is_running = False
+        self.is_running = False
 
     def current_values(self) -> DeviceValues:
         return self._current_values_buffer
@@ -78,19 +87,18 @@ class RegisterReaderThread(QThread):
         """
         logger.info('getting device state')
         try:
-            mode = self._read_registers(17, 1)[0]
+            mode = self._read_registers(self.registers.operating_mode)[0]
 
         except ReadRegistersError:
             logger.error('getting device state form buffer')
             mode = self._current_values_buffer.current_operating_mode
         return mode
 
-    def _read_registers(self, address: int, count: int) -> List[int] | None:
+    def _read_registers(self, register: Register) -> List[int] | None:
         """
-        Read 'count' registers starting from 'address' from device with 'slave_id' id
+        Read register(s) from device
         Raises ReadRegistersError if response is invalid
-        :param address: Starting register address
-        :param count: Amount of registers to read
+        :param register: Register dataclass with address and bits count
         :return: List of registers
         """
         logger.info('reading registers')
@@ -99,7 +107,7 @@ class RegisterReaderThread(QThread):
             self.modbus.connect()
             # self.modbus_connection_lost.emit()
 
-        response = self.modbus.read_input_registers(address, count, self.slave_id)
+        response = self.modbus.read_input_registers(register.address, register.count, self.slave_id)
         if response.isError():
             logger.error('lost connection to modbus device')
             self.modbus_connection_lost.emit()
@@ -118,23 +126,24 @@ class RegisterReaderThread(QThread):
         """
         logger.info('getting current temperature')
         try:
-            current_temperature = self._read_registers(2, 1)[0]
+            current_temperature = self._read_registers(self.registers.current_temperature)[0]
             logger.info(f'current temperature: {current_temperature}')
         except ReadRegistersError:
             logger.error('getting current temperature from buffer')
             current_temperature = self._current_values_buffer.current_temperature
         return current_temperature
 
-    def get_current_temperature_program(self):
+    def get_current_temperature_program(self) -> TemperatureProgram:
         """
-                Get current temperature program parameters or read from buffer
-                :return: TemperatureProgram object composed of fetched parameters
-                """
+        Get current temperature program parameters or read from buffer
+        :return: TemperatureProgram object composed of fetched parameters
+        """
         logger.info('getting current temperature program')
         try:
-            registers = self._read_registers(257, 4)
+            registers = self._read_registers(self.registers.temperature_program)
             program = TemperatureProgram(*registers)
 
+            # convert temperature to float
             program.target_temperature /= 10**program.point_position
 
         except ReadRegistersError:
@@ -144,9 +153,13 @@ class RegisterReaderThread(QThread):
         return program
 
     def get_current_point_position(self):
+        """
+        Get current temperature's (not target!) value point position
+        :return:
+        """
         logger.info('getting current temperature point position')
         try:
-            point_position = self._read_registers(0, 1)[0]
+            point_position = self._read_registers(self.registers.current_point_position)[0]
         except ReadRegistersError:
             logger.error('getting device state form buffer')
             point_position = self._current_values_buffer.current_point_position
