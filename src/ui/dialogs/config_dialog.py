e3008629fc3b75ecfc553dce1d6825268e5f99f0
v0es <markkikov@gmail.com>
2025-02-04 16:54:45 +0300

    changed input structure in config dialog

--- src/ui/dialogs/config_dialog.py
+++ src/ui/dialogs/config_dialog.py
@@ -1,3 +1,4 @@
+import numpy as np
 from PyQt6.QtWidgets import QDialog
 
 from .resource.config_dialog.config_dialog_ui import Ui_config_dialog
@@ -22,20 +23,17 @@ class ConfigDialog(QDialog, Ui_config_dialog):
         self.load_settings()
 
     def load_settings(self):
-        self.low_red_spinbox.setValue(self.analysis_settings.red_start)
-        self.up_red_spinbox.setValue(self.analysis_settings.red_end)
-        self.step_red_spinbox.setValue(self.analysis_settings.red_speed)
+        self.low_red_spinbox.setValue(self.analysis_settings.lower_red_bgr[2])
+        self.up_red_spinbox.setValue(self.analysis_settings.upper_red_bgr[2])
 
-        self.low_green_spinbox.setValue(self.analysis_settings.green_start)
-        self.up_green_spinbox.setValue(self.analysis_settings.green_end)
-        self.step_green_spinbox.setValue(self.analysis_settings.green_speed)
+        self.low_green_spinbox.setValue(self.analysis_settings.lower_red_bgr[1])
+        self.up_green_spinbox.setValue(self.analysis_settings.upper_red_bgr[1])
 
-        self.low_blue_spinbox.setValue(self.analysis_settings.blue_start)
-        self.up_blue_spinbox.setValue(self.analysis_settings.blue_end)
-        self.step_blue_spinbox.setValue(self.analysis_settings.blue_speed)
+        self.low_blue_spinbox.setValue(self.analysis_settings.lower_red_bgr[0])
+        self.up_blue_spinbox.setValue(self.analysis_settings.lower_red_bgr[0])
 
         self.cutoff_spinbox.setValue(self.analysis_settings.cut_off)
-        self.scaling_coeff_spinbox.setValue(self.analysis_settings.scaling)
+        self.scaling_coeff_dspinbox.setValue(self.analysis_settings.scaling)
         self.base_height_dspinbox.setValue(self.analysis_settings.base_height)
         self.height_gap_dspinbox.setValue(self.analysis_settings.height_gap)
 
@@ -44,23 +42,22 @@ class ConfigDialog(QDialog, Ui_config_dialog):
 
     def get_data(self):
         analysis_settings = AnalysisSettings(
-            self.low_red_spinbox.value(),
-            self.up_red_spinbox.value(),
-            self.step_red_spinbox.value(),
-
-            self.low_green_spinbox.value(),
-            self.up_green_spinbox.value(),
-            self.step_green_spinbox.value(),
-
-            self.low_blue_spinbox.value(),
-            self.up_blue_spinbox.value(),
-            self.step_blue_spinbox.value(),
-
-            self.cutoff_spinbox.value(),
-            self.scaling_coeff_spinbox.value(),
-            self.base_height_dspinbox.value(),
-            self.height_gap_dspinbox.value()
-
+            lower_red_bgr=np.array([
+                self.low_blue_spinbox.value(),
+                self.low_green_spinbox.value(),
+                self.low_red_spinbox.value()
+            ]),
+            upper_red_bgr=np.array([
+                self.up_blue_spinbox.value(),
+                self.up_green_spinbox.value(),
+                self.up_red_spinbox.value()
+            ]),
+            search_iterations=1,
+            cut_off=self.cutoff_spinbox.value(),
+            scaling=self.scaling_coeff_dspinbox.value(),
+            base_height=self.base_height_dspinbox.value(),
+            height_gap=self.height_gap_dspinbox.value(),
+            width_gap=200
         )
 
         polling_settings = PollingSettings(
